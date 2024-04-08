f4107dbd22c1820d15e6e8fff47c6e44dddaadf7
tolga0001 <tolga_2001.fb@hotmail.com>
2024-04-08 21:20:47 +0300

    some problems are added

---
+++ src/deneme.java
@@ -0,0 +1,7 @@
+public class deneme {
+    public static void main(String[] args) {
+        int shiftAmount = 1, lastCol = 2;
+        int shiftedRowAmount = shiftAmount / (lastCol + 1);
+        System.out.println(shiftedRowAmount);
+    }
+}
---
+++ src/sorting.java
@@ -0,0 +1,5 @@
+public class sorting {
+    public static void main(String[] args) {
+        System.out.println(3>>4);
+    }
+}
